1446ce25f275ec9351e556d13e2b6848b97b4d8a
Inigo Ramli <inigo.ramli@ui.ac.id>
2021-11-15 03:42:08 +0700

    Fix conversion script

--- utils/convert_to_distilbert_for_masked_lm.py
+++ utils/convert_to_distilbert_for_masked_lm.py
@@ -1,6 +1,6 @@
 import argparse
-from model.modeling_kepler import KeplerModel
-from transformers import save_pretrained
+from model.modeling_kepler import KeplerModel, KeplerConfig
+from transformers import DistilBertModel, DistilBertForMaskedLM
 
 parser = argparse.ArgumentParser(description="Extract the encoder and MLM head of KEPLER model")
 parser.add_argument('--kepler', help="Path to pretrained KEPLER model")
@@ -8,6 +8,9 @@ parser.add_argument('--distilbert', help="Destination for the new DistilBert mod
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    kepler = KeplerModel.from_pretrained(args.kepler)
+    mlm_head = DistilBertForMaskedLM.from_pretrained('cahya/distilbert-base-indonesian')
+    encoder = mlm_head.distilbert
+    config = KeplerConfig()
+    kepler = KeplerModel.from_pretrained(args.kepler, encoder, mlm_head)
     distilbert_for_masked_lm = kepler.mlm_head
     distilbert_for_masked_lm.save_pretrained(args.distilbert)
